820bf8fa757bf200fba90f20bf17292ae75ced0f
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-03-22 15:55:35 +0100

    37

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -4,7 +4,8 @@ public class Franc extends Money{
         this.amount  = amount;
     }
 
-    Franc times(int multiplier){
+    Money times(int multiplier){
+
         return new Franc(amount * multiplier);
     }
 
