de8039ed6cc7af979ca5fde0518dcaa602fc0027
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-03-25 19:43:04 +0100

    44 fixes #16

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,20 +1,13 @@
 public class Franc extends Money{
 
-    private String currency;
-
     Franc(int amount, String currency){
 
-        this.amount  = amount;
-        this.currency = currency;
+        super(amount,currency);
     }
 
     Money times(int multiplier){
 
         return Money.franc(amount * multiplier);
-    }
-
-    String currency(){
-        return currency;
     }
 
 }
