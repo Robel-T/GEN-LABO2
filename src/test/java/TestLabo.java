37ecc2b20aaaed9a75c7921424a89cf71b916259
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-03-27 11:23:19 +0100

    66 fixes #1

--- src/test/java/TestLabo.java
+++ src/test/java/TestLabo.java
@@ -78,7 +78,7 @@ public class TestLabo {
 
     @Test
     public void testMixedAddition(){
-        Money fiveBucks = Money.dollar(5);
+        Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF","USD",2);
