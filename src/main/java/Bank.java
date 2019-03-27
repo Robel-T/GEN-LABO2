4c52964582db4ec60a867d01da3cdf16dad856a8
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-03-27 11:00:53 +0100

    62

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,4 +1,9 @@
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to){
         return source.reduce(this,to);
     }
